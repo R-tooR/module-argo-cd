d0e1ad3aabd8ab0f6a34cd0f9347dfb4615bc88c
R-tooR <arturbugaj37@gmail.com>
2022-05-24 15:42:35 +0200

    temporarily exclude helm

--- main.tf
+++ main.tf
@@ -5,24 +5,25 @@ provider "kubernetes" {
   client_key             = base64decode(var.kubernetes_client_key)
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = var.kubernetes_cluster_endpoint
-    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
-    client_certificate     = base64decode(var.kubernetes_client_certificate)
-    client_key             = base64decode(var.kubernetes_client_key)
-  }
-}
+#provider "helm" {
+#  kubernetes {
+#    host                   = var.kubernetes_cluster_endpoint
+#    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+#    client_certificate     = base64decode(var.kubernetes_client_certificate)
+#    client_key             = base64decode(var.kubernetes_client_key)
+#  }
+#}
 
 resource "kubernetes_namespace" "example" {
   metadata {
     name = "argo"
   }
 }
-resource "helm_release" "argocd" {
-  chart      = "argo-cd"
-  name       = "msur"
-  repository = "https://argoproj.github.io/argo-helm"
-  namespace  = "argo"
-}
+#resource "helm_release" "argocd" {
+#  chart      = "argo-cd"
+#  name       = "msur"
+#  repository = "https://argoproj.github.io/argo-helm"
+#  namespace  = "default"
+##  namespace  = "argo"
+#}
 
